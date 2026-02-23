820fd55a9e27df77c3cd3816439cd8826d92326f
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2026-02-23 20:09:20 -0300

    fix: fase 3

--- src/main/java/org/fase3/fase3.java
+++ src/main/java/org/fase3/fase3.java
@@ -1,4 +1,36 @@
 package org.fase3;
 
+import java.util.Scanner;
+
 public class fase3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] notas = new int[5];
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Digite a nota " + (i + 1) + ": ");
+            notas[i] = scanner.nextInt();
+        }
+
+        int maiorNota = notas[0];
+        int menorNota = notas[0];
+        int soma = 0;
+
+        for (int nota : notas) {
+            if (nota > maiorNota) {
+                maiorNota = nota;
+            }
+            if (nota < menorNota) {
+                menorNota = nota;
+            }
+            soma += nota;
+        }
+        
+        double media = (double) soma / notas.length;
+
+        System.out.println("Maior nota: " + maiorNota);
+        System.out.println("Menor nota: " + menorNota);
+        System.out.println("Média: " + media);
+    }
 }
