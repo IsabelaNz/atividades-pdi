46acb901b0af7c9135b6623715eedcbf4258e4bc
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2026-02-23 17:32:28 -0300

    fix: fase 2

--- src/main/java/org/fase2/fase2.java
+++ src/main/java/org/fase2/fase2.java
@@ -1,4 +1,19 @@
 package org.fase2;
 
+import java.util.Scanner;
+
 public class fase2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Digite um número para ver a tabuada");
+        numero = scanner.nextInt();
+
+        int i;
+        for (i = 0; i <= 9; i++) {
+            System.out.println(numero + " x " + i + " = " + numero * i);
+        }
+    }
 }
